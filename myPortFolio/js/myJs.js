ba3f314947139ffa2bafa7abdbffcc6b98b98442
JanDesigner <Jpbatino@yahoo.com.ph>
2018-05-23 21:45:34 +0800

    Add files via upload

---
+++ myPortFolio/js/myJs.js
@@ -0,0 +1,39 @@
+
+function html() {
+	document.getElementById("title").innerHTML="HTML";
+	document.getElementById("skillDescription").innerHTML="Hypertext Markup Language is the standard markup language for creating web pages and web applications. With Cascading Style Sheets and JavaScript, it forms a triad of cornerstone technologies for the World ";
+
+}
+
+function css() {
+
+	
+}
+
+
+function java() {
+	document.getElementById("title").innerHTML="HTML";
+	document.getElementById("skillDescription").innerHTML="Basic Knowledge of HTML5";
+	
+}
+
+
+function javascript() {
+	document.getElementById("title").innerHTML="HTML";
+	document.getElementById("skillDescription").innerHTML="Basic Knowledge of HTML5";
+	
+}
+
+
+function spring() {
+	document.getElementById("title").innerHTML="HTML";
+	document.getElementById("skillDescription").innerHTML="Basic Knowledge of HTML5";
+	
+}
+
+
+function springboot() {
+	document.getElementById("title").innerHTML="HTML";
+	document.getElementById("skillDescription").innerHTML="Basic Knowledge of HTML5";
+	
+}
